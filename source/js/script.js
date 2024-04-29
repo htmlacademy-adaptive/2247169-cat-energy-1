ceb594566c5617e71cf3e744789c53ed33e6716e
OlgaKosukhina <heeeelga@gmail.com>
2024-04-30 00:04:26 +0300

    Изменения в блоке example, реализация слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+// Showing and hiding menu in mobile
+
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 
@@ -12,3 +14,82 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+// Compare slider
+const compareSlider = document.querySelector('.compare-slider');
+const beforeContainer = document.querySelector('.compare-slider__picture--before');
+const beforeImage = document.querySelector('.compare-slider__image--before');
+const sliderHandle = document.querySelector('.compare-slider__dragme');
+const body = document.body;
+
+let isActive = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  let width = compareSlider.offsetWidth;
+  beforeImage.style.width = `${width}px`;
+});
+
+sliderHandle.addEventListener('mousedown', () => {
+  isActive = true;
+});
+
+body.addEventListener('mouseup', () => {
+	isActive = false;
+});
+
+body.addEventListener('mouseleave', () => {
+	isActive = false;
+});
+
+const beforeAfterSlider = (x) => {
+	let shift = Math.max(0, Math.min(x, compareSlider.offsetWidth));
+	beforeContainer.style.width = `${shift}px`;
+	sliderHandle.style.left = `${shift}px`;
+};
+
+const pauseEvents = (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	return false;
+};
+
+body.addEventListener('mousemove', (e) => {
+	if (!isActive) {
+		return;
+	}
+
+	let x = e.pageX;
+	x -= compareSlider.getBoundingClientRect().left;
+	beforeAfterSlider(x);
+	pauseEvents(e);
+});
+
+sliderHandle.addEventListener('touchstart', () => {
+	isActive = true;
+});
+
+body.addEventListener('touchend', () => {
+	isActive = false;
+});
+
+body.addEventListener('touchcancel', () => {
+	isActive = false;
+});
+
+body.addEventListener('touchmove', (e) => {
+	if (!isActive) {
+		return;
+	}
+
+  let x;
+
+  let i;
+  for (i = 0; i < e.changedTouches.length; i++) {
+  	x = e.changedTouches[i].pageX;
+  }
+
+  x -= compareSlider.getBoundingClientRect().left;
+
+  beforeAfterSlider(x);
+  pauseEvents(e);
+}, { passive: false });
