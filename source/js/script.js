54afb6504bdf5ac20bda1116ba9bbdf34c3e04f4
OlgaKosukhina <heeeelga@gmail.com>
2024-04-30 21:56:17 +0300

    Исправления для линтера

--- source/js/script.js
+++ source/js/script.js
@@ -34,58 +34,58 @@ sliderHandle.addEventListener('mousedown', () => {
 });
 
 body.addEventListener('mouseup', () => {
-	isActive = false;
+  isActive = false;
 });
 
 body.addEventListener('mouseleave', () => {
-	isActive = false;
+  isActive = false;
 });
 
 const beforeAfterSlider = (x) => {
-	let shift = Math.max(0, Math.min(x, compareSlider.offsetWidth));
-	beforeContainer.style.width = `${shift}px`;
-	sliderHandle.style.left = `${shift}px`;
+  let shift = Math.max(0, Math.min(x, compareSlider.offsetWidth));
+  beforeContainer.style.width = `${shift}px`;
+  sliderHandle.style.left = `${shift}px`;
 };
 
 const pauseEvents = (e) => {
-	e.stopPropagation();
-	e.preventDefault();
-	return false;
+  e.stopPropagation();
+  e.preventDefault();
+  return false;
 };
 
 body.addEventListener('mousemove', (e) => {
-	if (!isActive) {
-		return;
-	}
+  if (!isActive) {
+    return;
+  }
 
-	let x = e.pageX;
-	x -= compareSlider.getBoundingClientRect().left;
-	beforeAfterSlider(x);
-	pauseEvents(e);
+  let x = e.pageX;
+  x -= compareSlider.getBoundingClientRect().left;
+  beforeAfterSlider(x);
+  pauseEvents(e);
 });
 
 sliderHandle.addEventListener('touchstart', () => {
-	isActive = true;
+  isActive = true;
 });
 
 body.addEventListener('touchend', () => {
-	isActive = false;
+  isActive = false;
 });
 
 body.addEventListener('touchcancel', () => {
-	isActive = false;
+  isActive = false;
 });
 
 body.addEventListener('touchmove', (e) => {
-	if (!isActive) {
-		return;
-	}
+  if (!isActive) {
+    return;
+  }
 
   let x;
 
   let i;
   for (i = 0; i < e.changedTouches.length; i++) {
-  	x = e.changedTouches[i].pageX;
+    x = e.changedTouches[i].pageX;
   }
 
   x -= compareSlider.getBoundingClientRect().left;
